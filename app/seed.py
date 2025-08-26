7c3b5c723ac2e83fc1d52e8caea9bb7d44b5888e
Mercy <kiqsmercy@gmail.com>
2025-08-27 00:54:10 +0300

    add seed function to populate initial planter data in seed.py

---
+++ app/seed.py
@@ -0,0 +1,51 @@
+from models.base import session
+from models.planter import Planter
+from datetime import datetime
+
+def seed():
+    # This clears existing data to avoid duplicates during seeding
+    session.query(Planter).delete()
+
+    # Samples of planters that could be added
+    planters = [
+        Planter(
+            name="Alice Green",
+            location="Nakuru",
+            contact_info="0712345678",
+            plant_type="Flowers",
+            experience_level="Beginner",
+            experience_months=2,
+            farm_size="Garden",
+            preferred_tools="Trowel, Pruners",
+            created_at=datetime.now()
+        ),
+        Planter(
+            name="Jeff Njuguna",
+            location="Eldoret",
+            contact_info="0723456789",
+            plant_type="Tress",
+            experience_level="Expert",
+            experience_months=3,
+            farm_size="4 acres",
+            preferred_tools="Pruning saw, Tractor ",
+            created_at=datetime.now()
+        ),
+         Planter(
+            name="Mary Kamba",
+            location="Kisumu",
+            contact_info="0798765432",
+            plant_type="Vegetables",
+            experience_level="Intermediate",
+            experience_months=12,
+            farm_size="1/2 Acre",
+            preferred_tools="Hoe, Water Pump",
+            created_at=datetime.now()
+        ),
+    ]
+
+    session.add_all(planters)
+    session.commit()
+    print("✅ Seed data was added successfully!")
+
+if __name__ == "__main__":
+    seed()
