86ef92ab4001f6edc5004c0e538f855c3d633a8f
Mercy <kiqsmercy@gmail.com>
2025-08-29 11:18:46 +0300

    Updates

--- models/base.py
+++ models/base.py
@@ -3,15 +3,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Database path
 DB_PATH = os.path.join(os.path.dirname(__file__), "..", "database", "plantbase.db")
 DB_PATH = os.path.abspath(DB_PATH)
 
-# Engine and session
-engine = create_engine(f"sqlite:///plantbase.db", echo=False)  # echo=True for debug
+engine = create_engine(f"sqlite:///plantbase.db", echo=False) 
 
 Session = sessionmaker(bind=engine)
 session = Session()
 
-# Base declarative class
 Base = declarative_base()
